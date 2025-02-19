be634a59a3ff4095143a1994c63d92043927575d
STEPHAN F <115410679+stephan231@users.noreply.github.com>
2025-02-19 23:32:35 +0530

    Update NavbarBtn.jsx
    Nav change

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -5,7 +5,7 @@ const NavbarBtn = () => {
   return (
     <button className=" px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
       <Link spy={true} smooth={true} duration={500} offset={-120} to="contact">
-        Hire Me
+        Contact Me
       </Link>
       <div className="sm:hidden md:block">
         <LuArrowDownRight />
